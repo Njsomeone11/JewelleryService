d474edc7e21dc144732d4c864173d3fa535a15b5
nevin <nevinjoseph847@gmil.com>
2026-03-31 00:29:06 +0400

    Initial commit

---
+++ src/main/java/com/shop/jewelleryservice/JewelleryServiceApplication.java
@@ -0,0 +1,13 @@
+package com.shop.jewelleryservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JewelleryServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JewelleryServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/shop/jewelleryservice/JewelleryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shop.jewelleryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JewelleryServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
